b6d212dc78477d1f79511edf4082f052eee84405
Romilly Cocking <romilly.cocking@gmail.com>
2020-04-03 08:18:22 +0100

    Starting on the Arduino SPI multi-sensor code.

--- plan/journal.md
+++ plan/journal.md
@@ -1,4 +1,12 @@
 # Project journal for multi-VL53L0X
 
+I'm experimenting to see how easy it is to connect multiple VL53L0X sensors to a Pi, Nano or Adafruit feather.
+
+I have two approaches in mind:
+
+1. Use an Arduino slave that selectively enables the chips and gives each a unique I2C address
+1. Use an I2C multiplexer
+
+
 
 
---
+++ src/mux-test.py
@@ -0,0 +1,22 @@
+# This example shows using two  sensors attached to TCA9548A channels 0 and 1.
+import time
+import board
+import busio
+import adafruit_tca9548a
+import adafruit_vl53l0x
+
+# Create I2C bus as normal
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the TCA9548A object and give it the I2C bus
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+# For each sensor, create it using the TCA9548A channel instead of the I2C object
+tof0 = adafruit_vl53l0x.VL53L0X(tca[0])
+tof1 = adafruit_vl53l0x.VL53L0X(tca[1])
+
+# Main loop will read the range and print it every second.
+while True:
+    print("Range for tof0: {0}mm".format(tof0.range))
+    print("Range for tof1: {0}mm".format(tof1.range))
+    time.sleep(1.0)
